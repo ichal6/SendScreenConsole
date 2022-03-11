2ca210a17be3bb2f46b2b5201acfc803672e8666
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-03-11 11:56:48 +0100

    Delete unused code

--- main.py
+++ main.py
@@ -3,29 +3,21 @@ from flask import Flask, render_template, Response
 import cv2
 import numpy as np
 import pyautogui
-from pynput.mouse import Button, Controller
+from pynput.mouse import Controller
 import time
 
 app = Flask(__name__)
 
 
-def gen_frames():  # generate frame by frame from camera
+def gen_frames():  # generate frame by frame
     mouse = Controller()
-    cursor = load_cursor()
+    cursor = load_cursor().convert('RGBA')
     while True:
         start = time.time()
         # Take screenshot using PyAutoGUI
         img = pyautogui.screenshot()
 
-        c_mode = img.mode
-
-        cursor_array = np.array(cursor)
-
-        cursor = cursor.convert('RGBA')
-
-        # cursor_RGB = cv2.cvtColor(cursor_array, cv2.COLOR_)
-
-        img.paste(cursor, (0,0), mask=cursor)
+        img.paste(cursor, mouse.position, mask=cursor)
 
         # Convert the screenshot to a numpy array
         frame = np.array(img)
@@ -34,12 +26,11 @@ def gen_frames():  # generate frame by frame from camera
         # RGB(Red, Green, Blue)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        mouse_position = mouse.position
-
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
         end = time.time()
         elapsed_time = end - start
 
@@ -50,6 +41,7 @@ def load_cursor():
         return image
     except Exception as e:
         print(e)
+
 
 @app.route('/video_feed')
 def video_feed():
@@ -67,11 +59,11 @@ if __name__ == '__main__':
     host = '192.168.1.103'
     port = '5000'
 
-    print("Ustawienia domyślne:", f'host = {host}', f'port = {port}')
-    answer = input("Czy zmienić ustawienia? [T/N] ").lower()
-
-    if answer == 'T':
-        host = input("Proszę podać adres hosta: ")
-        port = input("Proszę podać numer portu: ")
+    # print("Ustawienia domyślne:", f'host = {host}', f'port = {port}')
+    # answer = input("Czy zmienić ustawienia? [T/N] ").lower()
+    #
+    # if answer == 'T':
+    #     host = input("Proszę podać adres hosta: ")
+    #     port = input("Proszę podać numer portu: ")
 
     app.run(host="192.168.1.103", port=5000, debug=False)
