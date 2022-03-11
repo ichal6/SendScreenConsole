a2891d1dfefbe5bd3f7a05903fe64d20dc90dd3b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-03-11 11:49:46 +0100

    Append cursor on screen

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import PIL
 from flask import Flask, render_template, Response
 import cv2
 import numpy as np
 import pyautogui
+from pynput.mouse import Button, Controller
+import time
 
 app = Flask(__name__)
 
 
 def gen_frames():  # generate frame by frame from camera
+    mouse = Controller()
+    cursor = load_cursor()
     while True:
+        start = time.time()
         # Take screenshot using PyAutoGUI
         img = pyautogui.screenshot()
+
+        c_mode = img.mode
+
+        cursor_array = np.array(cursor)
+
+        cursor = cursor.convert('RGBA')
+
+        # cursor_RGB = cv2.cvtColor(cursor_array, cv2.COLOR_)
+
+        img.paste(cursor, (0,0), mask=cursor)
 
         # Convert the screenshot to a numpy array
         frame = np.array(img)
@@ -18,11 +34,22 @@ def gen_frames():  # generate frame by frame from camera
         # RGB(Red, Green, Blue)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+        mouse_position = mouse.position
+
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        end = time.time()
+        elapsed_time = end - start
 
+
+def load_cursor():
+    try:
+        image = PIL.Image.open('cursors/arrow.gif')
+        return image
+    except Exception as e:
+        print(e)
 
 @app.route('/video_feed')
 def video_feed():
